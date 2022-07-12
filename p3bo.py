40926d4c295675d8ee738134c14f4fa774450b3e
Daniel Danciu <daniel@cradle.bio>
2022-07-12 17:29:49 +0200

    More fleshing out

--- p3bo.py
+++ p3bo.py
@@ -1,1 +1,27 @@
 """ This is the file that contains the main P3BO method that you are supposed to implement. """
+from typing import List
+
+import flexs.explorer
+from flexs.optimizers.random import Random
+from flexs.optimizers.adalead import Adalead
+from flexs.optimizers.genetic_algorithm import GeneticAlgorithm
+
+
+class P3bo:
+    def __init__(self, explorers: List[flexs.explorer.Explorer]):
+        self.explorers = explorers
+
+
+
+
+
+def main():
+    adalead = ...
+    ga = ...
+    random = ...
+    p3bo = P3bo([random, ga, adalead])
+
+    p3bo.optimize()
+
+if __name__ == "__main__":
+    main()
